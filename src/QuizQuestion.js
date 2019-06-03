b1da7dd872f48973fa666c1611a83bd29999c488
Mouloud MOUALHI <mmoualhi-prestataire@vente-privee.com>
2019-06-03 15:43:35 +0200

    Displaying Quiz Questions and Handling Events

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,10 +4,10 @@ import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component {
 
-
-    constructor(props) {
-        super(props);
-        this.state = { quiz_position: 1 }
+    handleClick(buttonText) {
+        if (buttonText === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler()
+        }
     }
 
     render() {
@@ -18,7 +18,11 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+                        {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                            return <QuizQuestionButton key={index} button_text={answer_option}
+                                clickHandler={this.handleClick.bind(this)} />
+                        })}
+
                     </ul>
                 </section>
             </main>
