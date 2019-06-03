b916e218c53b2e8b680a42ac0c4879f787732146
Mouloud MOUALHI <mmoualhi-prestataire@vente-privee.com>
2019-06-03 14:58:27 +0200

    Creating a QuizEnd Component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
 
 let quizData = require('./quiz_data.json');
 
@@ -13,8 +14,13 @@ class Quiz extends Component {
     }
 
     render() {
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
+        
         return (
-            <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]} />
+            <div>
+                {isQuizEnd ? <QuizEnd /> :
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+            </div>
         )
     }
 }
